6b73935b33264a478e47bd4e038e5204f6968c3b
rakkatak <anita@rakkatak.com>
2017-06-07 15:26:54 -0400

    changing paths

--- js/app.js
+++ js/app.js
@@ -12,11 +12,11 @@ leheraApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/leheras', {
-        templateUrl: '/partials/lehera-list.html',
+        templateUrl: 'partials/lehera-list.html',
         controller: 'LeheraListCtrl'
       }).
       when('/leheras/:leheraId', {
-        templateUrl: '/partials/lehera-detail.html',
+        templateUrl: 'partials/lehera-detail.html',
         controller: 'LeheraDetailCtrl'
       }).
       otherwise({
